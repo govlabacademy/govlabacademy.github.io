766ce78a82cef850d49c22e869372e6f92b36206
Arnaud Sahuguet <arnaud.sahuguet@gmail.com>
2015-01-07 17:32:45 -0500

    test for adding data into template context

---
+++ plugins/mycustomplugin.py
@@ -0,0 +1,13 @@
+from cactus.plugin_base import CactusPluginBase
+import os
+import json
+
+# file: plugins/mycustomplugin.py
+class MyCustomPlugin(CactusPluginBase):
+	def templateContext(self, *args, **kwargs):
+		from django.conf import settings
+		print settings
+		data_folder = "%s/../data" % "/".join(os.path.abspath(__file__).split('/')[:-1])
+		return {
+    	"cards": json.load(open("%s/cards.json" % data_folder))
+    }
