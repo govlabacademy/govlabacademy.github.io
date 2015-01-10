2e45d3bbd7fc58d404b762d1e15fd0839c68f048
claudioccm <gigeer@gmail.com>
2015-01-10 12:48:15 -0500

    Migration to StaticJinja

--- README.md
+++
@@ -1,4 +0,0 @@
-Academy
-=======
-
-Using cactus Refactored to generate the static files - https://github.com/randomknowledge/Cactus_Refactored
---
+++ data/courses.py
@@ -0,0 +1,24 @@
+"courses" = {
+	"course1": {
+		"name": "xxx",
+		"partner": "",
+		"date": "xxx",
+		"duration": "xxx",
+		"description": "xxx",
+		"faculty_members":["faculty_name", "faculty_name"],
+		"links": ["link1", "link2"],
+		"certificates":["online", "blended", "in_person", "for_credit", "for_certificate"],
+		"is_active": False
+	},
+	"course2": {
+		"name": "xxx",
+		"partner": "",
+		"date": "xxx",
+		"duration": "xxx",
+		"description": "xxx",
+		"faculty_members":["faculty_name", "faculty_name"],
+		"links": ["link1", "link2"],
+		"certificates":["online", "blended", "in_person", "for_credit", "for_certificate"],
+		"is_active": False
+	}
+}
