6f64cc76f746198f1ac6d4a3efb621d55852c103
Arnaud Sahuguet <arnaud.sahuguet@gmail.com>
2015-01-13 16:51:23 -0500

    people.yaml

--- build.py
+++ build.py
@@ -9,9 +9,10 @@ searchpath  = os.path.join(cwd, "templates")
 outputpath  = os.path.join(cwd, "site")
 
 # We load the data we want to use in the templates.
-PEOPLE = json.load(open('data/cards.json'))
+#PEOPLE = json.load(open('data/cards.json'))
+PEOPLE    = yaml.load(open('data/people.yaml'))
 WORKSHOPS = yaml.load(open('data/workshops.yaml'))
-CLINICS = yaml.load(open('data/clinics.yaml'))
+CLINICS   = yaml.load(open('data/clinics.yaml'))
 
 def loadAcademyData():
 	return { 'people': PEOPLE,
@@ -21,6 +22,8 @@ def loadAcademyData():
 
 # We define some filters we want to use in the templates.
 def containsTag(x, y):
+	if x['tags'] is None:
+		return None
 	return x if y in x['tags'] else None
 
 filters = {
